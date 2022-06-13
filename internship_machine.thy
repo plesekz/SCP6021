35a345b4738c60eea4a5fcd52aafa1d7f26da052
Teddy Cameron-Burke <75274513+TeddyCB@users.noreply.github.com>
2022-06-13 17:41:09 +0100

    Teddy Branch created, have simple s1 loop and basic proof structure done

--- internship_machine.thy
+++ internship_machine.thy
@@ -16,12 +16,34 @@ INPUT::INPUT
 STATE:: State
 OUT:: OUT
 
+(* transition function *)
+fun t:: "State \<times> INPUT \<Rightarrow> State \<times> OUT" where
+"t (S_1, 0) = (S_1, False)"|
+"t (S_1, n) = (S_2, True) "|
+"t (S_2, 0) = (S_1, False) "|
+"t (S_2, n) = (S_2, True)"
 
-consts t:: "State \<times> INPUT \<Rightarrow> State \<times> OUT"
 
 
-definition paths:: "(nat\<Rightarrow>STEP) set" where
+(*set of all paths*)
+definition paths:: "(nat \<Rightarrow> STEP) set" where
 "paths \<equiv> {p::(nat \<Rightarrow> STEP). STATE(p(0)) = S_1 \<and> (\<forall>n. \<exists>i. t(STATE(p n),i) = (STATE(p(n+1)), OUT(p n)))}"
 
+(*very simple S_1 loop*)
+definition path:: "nat \<Rightarrow> STEP" where
+"path n = \<lparr> INPUT = 0,STATE = S_1,OUT = False \<rparr>"
+
+
+lemma path_init: "STATE(path(0)) = S_1"
+  by (simp add: path_def)
+
+lemma path_induct: "(\<forall>n. \<exists>i. t(STATE(path n),i) = (STATE(path(Suc(n))), OUT(path n)))"
+proof
+  fix i
+  let ?case = "n = 0 \<Longrightarrow> \<exists>i. t(STATE(path n),i) = (STATE(path(Suc(n))), OUT(path n))"
+  assume "n = 0"
+  have 1: "t(STATE(path 0), i) = t(S_1,i)" using path_def by simp
+  then have "... = t(S_1, 0)" try
+qed
 
 end
