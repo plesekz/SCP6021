0e0f713f79fe7d0217b70e26332422bc69d5f77a
Ellie <EllieRHudson@gmail.com>
2022-06-13 17:20:21 +0100

    Added Zero-Count Machine and Relabelled

--- internship_machine.thy
+++ internship_machine.thy
@@ -3,20 +3,40 @@ theory internship_machine
 begin
 
 datatype State = S_1 | S_2 
-
-definition \<Sigma> :: "State set" where
-"\<Sigma> = {S_1, S_2}"
-
-
 type_synonym INPUT = nat
 type_synonym OUT =  bool
+
+(* Q: Finite set of States *)
+definition Q :: "State set" where
+"Q = {S_1, S_2}"
+
+(* \<Sigma>: Finite set of Inputs | Alphabet *)
+definition \<Sigma> :: "INPUT set" where
+"\<Sigma> = {0, 1}"
 
 record STEP =
 INPUT::INPUT
 STATE:: State
 OUT:: OUT
 
-(* transition function *)
+(* Transition functions (t/\<delta> ) *)
+
+(* \<delta> Function for Odd/Even number of Zeros Machine: S_1 = Even / S_2 = Odd *)
+fun \<delta>:: "State \<times> INPUT \<Rightarrow> State" where
+"\<delta> (S_1, 0) = S_2" |
+"\<delta> (S_1, n) = S_1" |
+"\<delta> (S_2, 0) = S_1" |
+"\<delta> (S_2, n) = S_2"
+
+(* run the automata using word (Input list) *)
+fun run:: "State \<Rightarrow> INPUT list \<Rightarrow> State" where
+"run st [] = st" |
+"run st (Cons x xs) = run (\<delta> (st, x)) xs"
+value "run S_1 [0,1,1,0]"
+value "run S_1 [1,1,0]"
+
+
+(* t function for Boolean Machine *)
 fun t:: "State \<times> INPUT \<Rightarrow> State \<times> OUT" where
 "t (S_1, 0) = (S_1, False)"|
 "t (S_1, n) = (S_2, True) "|
