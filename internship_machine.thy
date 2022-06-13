19b4e9f597be803d269915762d5af5a5efd3b702
Zdeněk Plešek <31187824+plesekz@users.noreply.github.com>
2022-06-13 16:51:31 +0100

    Update internship_machine.thy

--- internship_machine.thy
+++ internship_machine.thy
@@ -16,12 +16,38 @@ INPUT::INPUT
 STATE:: State
 OUT:: OUT
 
+(* transition function *)
+fun t:: "State \<times> INPUT \<Rightarrow> State \<times> OUT" where
+"t (S_1, 0) = (S_1, False)"|
+"t (S_1, n) = (S_2, True) "|
+"t (S_2, 0) = (S_1, False) "|
+"t (S_2, n) = (S_2, True)"
 
-consts t:: "State \<times> INPUT \<Rightarrow> State \<times> OUT"
 
 
+(*set of all paths*)
 definition paths:: "(nat\<Rightarrow>STEP) set" where
 "paths \<equiv> {p::(nat \<Rightarrow> STEP). STATE(p(0)) = S_1 \<and> (\<forall>n. \<exists>i. t(STATE(p n),i) = (STATE(p(n+1)), OUT(p n)))}"
 
+
+(*general path*)
+inductive p::"nat\<Rightarrow>STEP \<Rightarrow> bool" where
+"tmp = \<lparr>INPUT = i, STATE = S_1, OUT = snd(t(S_1, i))\<rparr>  \<Longrightarrow>  p 0 tmp"|
+"\<exists>i::nat. tmp = \<lparr> INPUT = i, STATE = s, OUT = snd(t(s,i)) \<rparr> \<Longrightarrow> p n tmp \<Longrightarrow> p (Suc n) = "
+
+
+
+lemma non_empty_paths:
+  assumes a: "paths = {}"
+shows "\<not>a"
+proof-
+  fix p::"nat\<Rightarrow>STEP"
+  let ?case1 = "STATE(p(0)) = S_1"
+  let ?case2 = "(\<forall>n. \<exists>i. t(STATE(p n),i) = (STATE(p(n+1)), OUT(p n)))"
+  
+
+
+  (*from paths_def have "(i,S_1,second(t (S_1, i))) \<in> paths" using t_def by simp*)
+qed
 
 end
