864fca24f00f6150dfb94ad220faefcc30fa0cf7
Teddy Cameron-Burke <75274513+TeddyCB@users.noreply.github.com>
2022-06-16 14:42:49 +0100

    def changed and lemmas fixed

--- internship_machine.thy
+++ internship_machine.thy
@@ -76,32 +76,30 @@ inductive group_interconnectivness:: "State set \<Rightarrow> bool" where
 
 (*set of all paths*)
 definition paths:: "(nat \<Rightarrow> STEP) set" where
-"paths \<equiv> {p::(nat \<Rightarrow> STEP). STATE(p(0)) = S_1 \<and> (\<forall>n. \<exists>i. t(STATE(p n),i) = (STATE(p(Suc(n))), OUT(p n)))}"
+"paths \<equiv> {p::(nat \<Rightarrow> STEP). STATE(p(0)) = S_1 \<and> (\<forall>n. t(STATE(p n),INPUT(p n)) = (STATE(p(Suc(n))), OUT(p n)))}"
 
 (*very simple S_1 loop*)
 definition specific_path:: "nat \<Rightarrow> STEP" where
 "specific_path n = \<lparr> INPUT = 0,STATE = S_1,OUT = False \<rparr>"
 
+lemma spec_path_cond1: "STATE(specific_path(0)) = S_1" using specific_path_def by auto
 
-lemma specific_path_init: "STATE(specific_path(0)) = S_1"
+lemma spec_path_cond2: "\<forall>n. t(STATE(specific_path n),INPUT(specific_path n)) = (STATE(specific_path(Suc(n))), OUT(specific_path n))"
   by (simp add: specific_path_def)
 
-lemma rhs_simp [simp]: " (STATE(specific_path(Suc(n))), OUT(specific_path n)) = (S_1, False)" using specific_path_def by simp
 
-
-lemma path_induct: "(\<forall>n. \<exists>i. t(STATE(specific_path n),i) = (STATE(specific_path(Suc(n))), OUT(specific_path n)))"
-proof (rule allI)
-  fix n
-  have 0: "t(STATE(specific_path n), 0) = (STATE(specific_path(Suc(n))), OUT(specific_path n))" using specific_path_def by simp
-  then show "\<exists>i. t (STATE (specific_path n), i) = (STATE (specific_path (Suc n)), OUT (specific_path n))" using exI by simp
+lemma path_in_paths: "specific_path \<in> paths"
+proof-
+  from specific_path_def have "specific_path n = \<lparr> INPUT = 0,STATE = S_1,OUT = False \<rparr>" by simp
+  then have 1: "STATE(specific_path 0) = S_1" by (simp add: spec_path_cond1)
+  then have 2: "\<forall>n. t(STATE(specific_path n),INPUT(specific_path n)) = (STATE(specific_path(Suc(n))), OUT(specific_path n))" using spec_path_cond2 by auto
+  hence "STATE(specific_path 0) = S_1 \<and> (\<forall>n. t(STATE(specific_path n),INPUT(specific_path n)) = (STATE(specific_path(Suc(n))), OUT(specific_path n)))" by (simp add: spec_path_cond1)
+  hence 3: "specific_path \<in> paths" using paths_def by auto
+  thus ?thesis.
 qed
 
-lemma path_in_paths: "specific_path \<in> paths" using path_induct specific_path_init paths_def
-  by blast
-
-
-lemma "paths \<noteq> {}" using path_in_paths
-  by blast
+lemma "paths \<noteq> {}"
+  using path_in_paths by auto
 
 lemma big_fish: "\<forall>q\<in>paths. \<forall>m. \<exists>n>m. \<exists>s_0. OUT(q n) = s_0" by auto
 
@@ -122,15 +120,85 @@ proof
     by simp
   hence "\<exists>n. \<exists> m. STATE(specific_path n) = STATE(specific_path m)"
     by auto
-  hence "specific_path \<in> paths \<and> (\<exists>n. \<exists> m. STATE(specific_path n) = STATE(specific_path m))" using path_in_paths by simp
+  hence "specific_path \<in> paths \<and> (\<exists>n. \<exists>m. STATE(specific_path n) = STATE(specific_path m))" using path_in_paths by simp
   hence 4: "specific_path \<in> cyclic_paths" using cyclic_paths_def by auto
   from 4 0 show "False" by auto
 qed
 
+lemma 45: "\<forall>p \<in> paths. \<forall>i. \<exists>k. t(STATE(p i), k) = (STATE(p (Suc i)), OUT(p i)) \<and> INPUT(p i) > 0 \<longrightarrow> t(STATE(p i), INPUT(p i)) = (STATE(p (Suc i)), OUT(p i))"
+  by simp
+
+lemma stays_in_s_2: "\<forall>p \<in> paths. STATE(p n) = S_1 \<and> INPUT(p n) > 0 \<and> (\<forall>j. j \<in> {n<..<m} \<longrightarrow> INPUT(p j) = 0)  \<and> i \<in> {n<..<m}\<longrightarrow> STATE(p i) = S_2"
+proof
+  fix p
+  assume base: "p\<in>paths"
+  show "STATE (p n) = S_1 \<and> 0 < INPUT (p n) \<and> (\<forall>j. j \<in> {n<..<m} \<longrightarrow> INPUT (p j) = 0) \<and> i \<in> {n<..<m} \<longrightarrow> STATE (p i) = S_2"
+  proof (induction i)
+      case 0
+      then show ?case
+        by simp
+    next
+      case (Suc i)
+      show ?case
+      proof
+        assume LHS: "STATE (p n) = S_1 \<and> 0 < INPUT (p n) \<and> (\<forall>j. j \<in> {n<..<m} \<longrightarrow> INPUT (p j) = 0) \<and> Suc i \<in> {n<..<m}"
+        then have a: "STATE (p n) = S_1" 
+        and b: "0 < INPUT (p n)"
+        and c: " (\<forall>j. j \<in> {n<..<m} \<longrightarrow> INPUT (p j) = 0)"
+        and d: "Suc i \<in> {n<..<m}" by auto
+        then consider "Suc i = Suc n" | "Suc i \<noteq> Suc n" by auto
+        then show "STATE (p (Suc i)) = S_2"
+        proof (cases)
+          case 1
+          (*to use IH, one must prove the preconditions, or find a falsity and use False \<longrightarrow> P*)
+          then have "i \<notin> {n<..<m}" using 1 d by simp
+          then have "STATE (p n) = S_1 \<and> 0 < INPUT (p n) \<and> (\<forall>j. j \<in> {n<..<m} \<longrightarrow> INPUT (p j) = 0) \<and> i \<in> {n<..<m} \<longleftrightarrow> False" by blast
+          thus ?thesis 
+        next
+          case 2
+          then have a_2: "STATE (p n) = S_1" 
+          and b_2:" 0 < INPUT (p n)"
+          and c_2: "(\<forall>j. j \<in> {n<..<m} \<longrightarrow> INPUT (p j) = 0)"
+            using LHS by auto
+          (*show i \<in> {n<..<m}*)
+          from d have e_2: "Suc i \<in> {n <..<m}" by auto
+          have "Suc i > Suc n"
+            using d "2" by fastforce
+          then have d_2: "i \<in> {n <..<m}"
+            using d by auto
+          from c_2 d_2 have f: "INPUT(p i) = 0" by blast
+          from a_2 b_2 c_2 d_2 have IH: "STATE(p i) = S_2"
+            using Suc by blast 
+          from c_2 e_2 have "INPUT(p (Suc i)) = 0" by blast
+          then have i_path: "\<exists>k. t(STATE(p i), k) = (STATE(p (Suc i)), OUT(p i))" using paths_def base by blast
+
+          then have "\<exists>k. t(STATE(p i), k) = t(STATE(p i), INPUT(p i))" using c_2 d_2
+            by blast
+          then have "t(STATE(p i), INPUT(p i)) = t(S_2, 0)" using f IH by simp
+          then have "... = (S_2, True)" by simp
+          
+          
+        
+          
+        qed
+    qed
+  qed
+qed
+
+  
 
 
-
-
+lemma "\<forall>p \<in> paths. STATE(p n) = S_1 \<and> INPUT(p n) > 0 \<and> (\<forall>j. j \<in> {n<..<m} \<longrightarrow> INPUT(p j) = 0) \<and> i \<in> {n<..<m} \<longrightarrow> OUT(p i) = True"
+proof
+  assume 0: "p \<in> paths"
+  assume 1: "STATE(p n) = S_1 \<and> INPUT(p n) > 0 \<and> (\<forall>j. j \<in> {n<..<m} \<longrightarrow> INPUT(p j) = 0) \<and> i \<in> {n<..<m} "
+  (*show OUT(p i)*)
+  from 0 1 have "\<exists>k. t(STATE(p j), k) = (STATE(p (Suc j)), OUT(p j))" using paths_def by blast
+  then have "t(S_2, 0) = (STATE(p (Suc j)), OUT(p j))"
+    by (metis "0" "1" State.distinct(1) stays_in_s_2)
+  then have "(S_2, True) = (STATE(p (Suc j)), OUT(p j))" by simp
+  then have "OUT(p j) = True" by simp
+  
 
 
 end
