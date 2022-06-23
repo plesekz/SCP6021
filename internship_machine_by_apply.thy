0ecf00eba8c6e968d01898d2572d38f2aa0c4ad6
Zdeněk Plešek <7denek.p1esek@gmail.com>
2022-06-23 16:51:04 +0100

    Update internship_machine_by_apply.thy

--- internship_machine_by_apply.thy
+++ internship_machine_by_apply.thy
@@ -35,18 +35,18 @@ lemma no_dead_ends: "\<forall>s \<in> \<Sigma>. \<exists>i j. fst(t(s,i)) = j"
 definition paths:: "(nat \<Rightarrow> STEP) set" where
 "paths \<equiv> {p::(nat \<Rightarrow> STEP). STATE(p(0)) = INITIAL_NODE \<and> (\<forall>n. t(STATE(p n),INPUT (p n)) = (STATE(p(Suc(n))), OUT(p n)))}"
 
-lemma stays_in_s_2: "\<forall>p \<in> paths. STATE(p n) = S_1 \<and> INPUT(p n) > 0 \<and> (\<forall>j. j \<in> {n<..<m} \<longrightarrow> INPUT(p j) = 0)  \<and> i \<in> {n<..<m}\<longrightarrow> STATE(p i) = S_2"
+lemma stays_in_s_2: "\<forall>p. p \<in> paths \<and> STATE(p n) = S_1 \<and> INPUT(p n) > 0 \<and> (\<forall>j. j \<in> {n<..<m} \<longrightarrow> INPUT(p j) = 0)  \<and> i \<in> {n<..<m}\<longrightarrow> STATE(p i) = S_2"
   apply (induction i,simp)
   apply (case_tac "Suc i = Suc n")
   apply (auto simp add: paths_def)
   apply (metis gr0_conv_Suc old.prod.inject t.simps(2))
   by (metis Pair_inject Suc_lessD less_antisym t.simps(3))
 
-lemma stays_in_s_2: "\<forall>p \<in> paths. STATE(p n) = S_1 \<and> INPUT(p n) > 0 \<and> (\<forall>j. j \<in> {n<..<m} \<longrightarrow> INPUT(p j) = 0)  \<and> i \<in> {n<..<m}\<longrightarrow> OUT(p i) = True"
-  apply (auto)
-  apply (simp add: paths_def)
-  apply()
-  
-  done
 
+  thm allE[OF stays_in_s_2]
+
+lemma stays_in_s_3: "\<forall>p \<in> paths. STATE(p n) = S_1 \<and> INPUT(p n) > 0 \<and> (\<forall>j. j \<in> {n<..<m} \<longrightarrow> INPUT(p j) = 0)  \<and> i \<in> {n<..<m}\<longrightarrow> OUT(p i) = True"
+  apply (rule, rule_tac x="p" and n1="n" and m1="m" and i1="i" in allE[OF stays_in_s_2])
+  apply (simp add: paths_def)
+  by (metis old.prod.inject t.simps(3))
 end
